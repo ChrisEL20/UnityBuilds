757e01aae47defb1185fcac12a8625b856c2284e
ChrisEL20 <chriselvisleisi@gmail.com>
2017-08-20 19:02:32 +0200

    Delete Revide and Helsana

--- Helsana/Unity/Assets/frameRate.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class frameRate : MonoBehaviour {
-    void Awake() {
-        Application.targetFrameRate = 90;
-    }
-}
